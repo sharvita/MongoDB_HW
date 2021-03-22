f423ce7b0ff25d1a15b781660ec10a46869e806b
Sharvita Paithankar <sharvitap123@gmail.com>
2021-03-22 15:48:28 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+const { Mongoose } = require("mongoose");
+
+const express = require("express"), app = express();
+homeController = require("./controllers/homeController"),
+errorController = require("./controllers/errorController"),
+layouts = require("express-ejs-layouts");
+subscribersContoller = require("./controllers/subscribersContoller")
+Mongoose.connect("mongodb://localhost:27017/confetti_suisine",
+                 {useNewUrlParser: true}); 
+
+app.set("port", process.env.PORT || 3000);
+
+app.set("view engine", "ejs");
+app.use(layouts);
+
+app.get("/", homeController.showIndex);
+
+app.use(express.static("public"))
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+
+app.use(express.json());
+
+app.get("/courses", homeController.showCourses);
+app.get("/subscribers", subscribersContoller.getAllSubscribers);
+app.get("/contact", subscribersContoller.getSubscriptionPage);
+app.post("/subscribe", subscribersContoller.saveSubscriber);
+//app.get("/contact", homeController.showSignUp);
+//app.post("/contact", homeController.postedSignUpForm);
+
+
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
+
+
+app.listen(app.get("port"), () => {
+    console.log(`Server is running on port: ${app.get("port")}`)
+
+});
