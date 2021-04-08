bfea03932997cac46ee3a0459bcf7159c102c38c
Sharvita Paithankar <sharvitap123@gmail.com>
2021-04-08 12:23:36 -0600

    Update errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,14 +1,23 @@
-const httpStatus = require("http-status-codes")
+"use strict";
 
-exports.pageNotFoundError = (req, res) => {
+const httpStatus = require("http-status-codes");
+
+module.exports = {
+  logErrors: (error, req, res, next) => {
+    console.error(error.stack);
+    next(error);
+  },
+
+  pageNotFoundError: (req, res) => {
     let errorCode = httpStatus.NOT_FOUND;
     res.status(errorCode);
     res.render("error");
-}
+  },
 
-exports.internalServerError = (error, req, res, next) => {
+  internalServerError: (error, req, res, next) => {
     let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
+    console.log(`ERROR occurred: ${error.stack}`);
     res.status(errorCode);
-    res.send(`${errorCode}, Sorry! Something went wrong with your application`);
-    console.error(error.stack)
-}
+    res.send(`${errorCode} | Sorry, our application is experiencing a problem!`);
+  }
+};
