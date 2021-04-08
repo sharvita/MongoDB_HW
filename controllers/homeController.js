d996e1200baecffc41d570bb3ddaf19c2af44b2f
Sharvita Paithankar <sharvitap123@gmail.com>
2021-04-08 12:23:02 -0600

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,35 +1,7 @@
-var courses = [
-    {
-        title: "Raspberry Cake",
-        cost: 50
-    },
-    {
-        title: "Artichoke",
-        cost: 50
-    },
-    {
-        title: "Burger",
-        cost: 100
-    }
+"use strict";
 
-]
-
-exports.showCourses = (req,res) => {
-    res.render("courses", {offeredCourses: courses});
-
-}
-
-exports.showSignUp = ( req, res) => {
-    res.render("contact")
-
-}
-
-exports.postedSignUpForm = ( req, res) => {
-    res.render("thanks")
-    
-}
-
-exports.showIndex = (req,res) => {
+module.exports = {
+  index: (req, res) => {
     res.render("index");
-
-}
+  }
+};
